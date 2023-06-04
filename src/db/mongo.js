c2a484e5c459efc281b9f6d0052c194d87c02380
RomanistHere <RomanistHere@pm.me>
2023-06-04 17:01:05 +0400

    fix: try static env

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
-import { dev } from "$app/environment";
+// import { dev } from "$app/environment";
+
+import { MONGO_URL } from "$env/static/private";
 
 export const startMongo = async () => {
-	const host = dev ? "http://localhost:5173" : "https://popupoff.org";
-	const resp = await fetch(`${host}/api/keys/getConnectStringDB`);
-	const { key } = await resp.json();
-	return mongoose.connect(key);
+	// console.log("heh");
+	// console.log(MONGO_URL);
+	// const host = dev ? "http://localhost:5173" : "http://127.0.0.1:8787/";
+	// console.log(host)
+	// const resp = await fetch(`${host}/api/keys/getConnectStringDB`);
+	// console.log(resp);
+	// const { key } = await resp.json();
+	// console.log(key);
+	return mongoose.connect(MONGO_URL);
 };
 
 const db = mongoose.connection;
 // eslint-disable-next-line no-console
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 export default db;

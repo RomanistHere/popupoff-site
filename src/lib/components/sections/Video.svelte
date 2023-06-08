33dd5361590d89ba20bf56ba674293a355860678
RomanistHere <RomanistHere@pm.me>
2023-06-08 14:45:21 +0400

    fix: start video when scrolled

--- src/lib/components/sections/Video.svelte
+++ src/lib/components/sections/Video.svelte
@@ -1,10 +1,45 @@
 <script>
+	import { onMount } from "svelte";
+
+	import Spinner from "$lib/components/Spinner.svelte";
+
 	let isYouTube = false;
+	let scrolledEnough = false;
+	let ref = null;
+
+	const observeTillElementMet = ({ action, element }) => {
+		if (!element) {
+			return;
+		}
+
+		const observer = new IntersectionObserver(entries => {
+			const first = entries[0];
+			if (first.isIntersecting) {
+				action();
+
+				observer.disconnect();
+			}
+		});
+
+		observer.observe(element);
+	};
+
+	onMount(() => {
+		observeTillElementMet({
+			action: () => {
+				scrolledEnough = true;
+			},
+			element: ref,
+		});
+	});
 </script>
 
 <p id="video" />
 
-<section class="min-h-screen py-16 bg-bright flex justify-center items-center text-dark">
+<section
+	bind:this={ref}
+	class="min-h-screen py-16 bg-bright flex justify-center items-center text-dark"
+>
 	<div class="max-w-7xl px-8 w-full text-center">
 		<h1 class="text-title">See in action</h1>
 
@@ -53,17 +88,45 @@
 					allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 					allowfullscreen
 				/>
-			{:else}
+			{:else if scrolledEnough}
 				<iframe
 					width="860"
 					height="490"
-					src="https://customer-vlbvou0ablsk8yf5.cloudflarestream.com/a767b42bc5d14395ae45d8c23fac6576/iframe?muted=true&preload=true&autoplay=true&poster=https%3A%2F%2Fcustomer-vlbvou0ablsk8yf5.cloudflarestream.com%2Fa767b42bc5d14395ae45d8c23fac6576%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+					src="https://customer-vlbvou0ablsk8yf5.cloudflarestream.com/a767b42bc5d14395ae45d8c23fac6576/iframe?muted=true&autoplay=true"
 					title="Cloudflare video player"
 					frameborder="0"
 					allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
 					allowfullscreen
 				/>
+			{:else}
+				<div class="max-w-2xl text-xl mt-12">
+					<p class="animation-1 transition-opacity">
+						Sorry to inform that our JavaScript code to show the video didn't work.
+						Currently we can't afford to show you the video without JS.
+						If you're still interested, see the video on <a href="https://www.youtube.com/watch?v=3jTKRCxLyPE" class="underline">YouTube</a>, enable JS or reload page.
+					</p>
+
+					<div class="relative block w-48 h-48 mx-auto">
+						<Spinner />
+					</div>
+				</div>
 			{/if}
 		</div>
 	</div>
 </section>
+
+<style>
+	@keyframes from-opacity {
+		0%,
+		80% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	.animation-1 {
+		animation: from-opacity 2.5s ease-in;
+	}
+</style>

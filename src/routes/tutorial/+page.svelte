e5c10ae048de961f10939da3e1f6b3e7c212292c
RomanistHere <RomanistHere@pm.me>
2023-06-22 19:26:18 +0400

    feat: start tutorial

---
+++ src/routes/tutorial/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<main class="bg-bright w-screen min-h-screen text-dark">
+	there's somethign about it
+</main>
+
+<section class="fixed inset-0 flex justify-center items-center backdrop-blur-sm">
+	<div class="max-w-3xl w-full text-center bg-dark px-4 py-8 shadow-lg text-lg">
+		<h1 class="text-3xl font-bold mb-8 text-accent">
+			Welcome to the interactive PopUpOFF tutorial.
+		</h1>
+		<p class="my-4">
+			Learn how to get the great experience by going through the tutorial.
+			<br>
+			You'll only need <strong class="text-accent">30 seconds</strong>.
+		</p>
+
+		<img
+			src="/animated/animated-moderate.webp"
+			alt=""
+			class="max-w-2xl mx-auto my-4"
+			width="720"
+			height="450"
+		>
+
+		<p class="my-4">
+			<strong class="text-accent">Step 1.</strong> Enable "Moderate" mode to bypass the popup you're seeing by either:
+		</p>
+		<ul class="leading-8">
+			<li>
+				a) Click the extension's icon at the top and press "Moderate" button
+			</li>
+			<li>
+				b) Right click anywhere on the page, find "PopUpOFF" and choose "Moderate"
+			</li>
+		</ul>
+	</div>
+</section>
